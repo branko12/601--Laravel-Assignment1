58e9b758800906ac5ce07fdeb34ab99b63c4e0e9
Your Name <you@example.com>
2015-11-24 15:31:06 +0100

    2cas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(TasksSeeder::class);
 
         Model::reguard();
     }
